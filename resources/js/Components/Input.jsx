725f5edcd159aeedef9afc98fc38c6802d756988
richieid23 <fitrarizki23@gmail.com>
2023-05-09 15:00:09 +0700

    feat: slicing sign in & sign up

---
+++ resources/js/Components/Input.jsx
@@ -0,0 +1,58 @@
+import { useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
+
+Input.propTypes = {
+    type: PropTypes.oneOf(['text', 'email', 'password', 'number', 'file']),
+    name: PropTypes.string,
+    value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    defaultValue: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    className: PropTypes.string,
+    variant: PropTypes.oneOf(['primary', 'primary-outline', 'error']),
+    autoComplete: PropTypes.string,
+    required: PropTypes.bool,
+    isFocused: PropTypes.bool,
+    handleChange: PropTypes.func,
+    placeholder: PropTypes.string,
+    isError: PropTypes.bool
+}
+
+export default function Input({
+    type = 'text',
+    name,
+    value,
+    defaultValue,
+    className = '',
+    variant = 'primary',
+    autoComplete,
+    required,
+    isFocused = false,
+    handleChange,
+    placeholder,
+    isError = false,
+    ...props
+}) {
+    const input = useRef();
+
+    useEffect(() => {
+        if (isFocused) {
+            input.current.focus();
+        }
+    }, []);
+
+    return (
+        <div className="flex flex-col items-start">
+            <input
+                {...props}
+                type={type}
+                name={name}
+                value={value}
+                className={`rounded-2xl bg-form-bg py-[13px] px-7 w-full input-${variant} ${className}`}
+                ref={input}
+                autoComplete={autoComplete}
+                required={required}
+                onChange={(e) => handleChange(e)}
+                placeholder={placeholder}
+            />
+        </div>
+    );
+};
---
+++ resources/js/Components/Label.jsx
@@ -0,0 +1,7 @@
+export default function Label({ forInput, value, className = '', children, ...props }) {
+    return (
+        <label {...props} htmlFor={forInput} className={`text-base block mb-2 ` + className}>
+            {value ? value : children}
+        </label>
+    );
+}
