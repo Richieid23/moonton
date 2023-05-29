f3252687387135c0abc1fe49845a68d5494c36bb
richieid23 <fitrarizki23@gmail.com>
2023-05-29 14:22:06 +0700

    feat: integrate midtrans

--- resources/js/Pages/User/Dashboard/SubscriptionPlan/Index.jsx
+++ resources/js/Pages/User/Dashboard/SubscriptionPlan/Index.jsx
@@ -3,15 +3,41 @@ import { Head, router } from "@inertiajs/react";
 import Authenticated from "@/Layouts/Authenticated/Index";
 import SubscriptionCard from "@/Components/SubscriptionCard";
 
-export default function SubscriptionPlan({auth, subscriptionPlans}) {
+export default function SubscriptionPlan({auth, subscriptionPlans, env}) {
     const onSelectSubscription = (id) => {
-        // alert(id);
-        router.post(route('user.dashboard.subscription-plan.user-subscribe', {subscriptionPlan: id}));
+        router.post(
+            route('user.dashboard.subscription-plan.user-subscribe', {subscriptionPlan: id}),
+            {},
+            {
+                only: ['userSubscription'],
+                onSuccess: ({props}) => {
+                    onSnapMidtrans(props.userSubscription)
+                }
+            }
+        );
+    };
+
+    const onSnapMidtrans = (userSubscription) => {
+        snap.pay(userSubscription.snap_token, {
+            onSuccess: function(result){
+                router.visit(route('user.dashboard.index'))
+              },
+              // Optional
+              onPending: function(result){
+                console.log(result);
+              },
+              // Optional
+              onError: function(result){
+                console.log(result);
+              }
+        })
     }
+
     return (
         <>
             <Head>
                 <title>Subscription Plan</title>
+                <script src="https://app.sandbox.midtrans.com/snap/snap.js" data-client-key={env.MIDTRANS_CLIENTKEY}></script>
             </Head>
             <Authenticated auth={auth}>
                 <div className="py-20 flex flex-col items-center">
